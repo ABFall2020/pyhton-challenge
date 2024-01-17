32d7829916c0efab1ac605aa4a8445d507bf81aa
ABFall2020 <142930665+ABFall2020@users.noreply.github.com>
2024-01-16 20:26:28 -0600

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,17 +6,17 @@ import csv
 file_path = "./Resources/budget_data.csv"
 df = pd.read_csv(file_path)
 
-# Calculate total number of months
+# Calculate total months
 total_months = len(df)
 
 # Calculate net total amount of "Profit/Losses"
 net_total = df["Profit/Losses"].sum()
 
-# Calculate changes in "Profit/Losses" and the average of those changes
+# Calculate change in "Profit/Losses" and average of the changes
 df["Profit/Losses Change"] = df["Profit/Losses"].diff()
 average_change = df["Profit/Losses Change"].mean()
 
-# Find the greatest increase and decrease in profits
+# Find greatest increase and decrease in profits
 greatest_increase = df[df["Profit/Losses Change"] == df["Profit/Losses Change"].max()]
 greatest_decrease = df[df["Profit/Losses Change"] == df["Profit/Losses Change"].min()]
 
@@ -27,7 +27,7 @@ greatest_increase_amount = greatest_increase.iloc[0]["Profit/Losses Change"]
 greatest_decrease_date = greatest_decrease.iloc[0]["Date"]
 greatest_decrease_amount = greatest_decrease.iloc[0]["Profit/Losses Change"]
 
-# Print the results
+# Print results
 print("Financial Analysis")
 print("-----------------------------")
 print(f"Total Months: {total_months}")
@@ -36,11 +36,11 @@ print(f"Average Change: ${round(average_change, 2)}")
 print(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase_amount})")
 print(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease_amount})")
 
-# Create a directory for the analysis if it doesn't exist
+# Create a directory for the analysis
 output_folder = "analysis"
 os.makedirs(output_folder, exist_ok=True)
 
-# Specify the file path for the output file
+# Specify file path for output file
 output_file_path = os.path.join(output_folder, "financial_analysis.txt")
 
 # Open a file for writing
@@ -53,6 +53,3 @@ with open(output_file_path, "w") as file:
     file.write(f"Average Change: ${round(average_change, 2)}\n")
     file.write(f"Greatest Increase in Profits: {greatest_increase_date} (${greatest_increase_amount})\n")
     file.write(f"Greatest Decrease in Profits: {greatest_decrease_date} (${greatest_decrease_amount})\n")
-
-# Print a message indicating successful export
-print(f"Financial analysis results have been exported to '{output_file_path}'")
