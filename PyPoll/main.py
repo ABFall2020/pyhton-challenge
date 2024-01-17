e45257fde8f0aae35c2fdb0e0ae8cf44d9054f4e
ABFall2020 <142930665+ABFall2020@users.noreply.github.com>
2024-01-16 20:27:56 -0600

    Update main.py

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,20 +7,20 @@ import csv
 file_path = "./Resources/election_data.csv"
 df = pd.read_csv(file_path)
 
-# Calculate the total number of votes cast
+# Calculate total votes cast
 total_votes = len(df)
 
-# Create a list of unique candidates
+# Create list of unique candidates
 candidates = df["Candidate"].unique()
 
-# Calculate the percentage of votes and total votes for each candidate
+# Calculate percentage of votes and total votes for each candidate
 candidate_votes = df["Candidate"].value_counts()
 percentage_votes = (candidate_votes / total_votes) * 100
 
-# Find the winner based on popular vote
+# Find winner based on popular vote
 winner = candidate_votes.idxmax()
 
-# Print the results
+# Print results
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {total_votes}")
@@ -32,11 +32,11 @@ print("-------------------------")
 print(f"Winner: {winner}")
 print("-------------------------")
 
-# Create a directory for the analysis if it doesn't exist
+# Create directory for the analysis
 output_folder = "analysis"
 os.makedirs(output_folder, exist_ok=True)
 
-# Specify the file path for the output file
+# Specify file path for the output file
 output_file_path = os.path.join(output_folder, "election_results.txt")
 
 # Open a file for writing
@@ -52,6 +52,3 @@ with open(output_file_path, "w") as file:
     file.write("-------------------------\n")
     file.write(f"Winner: {winner}\n")
     file.write("-------------------------\n")
-
-# Print a message indicating successful export
-print(f"Results have been exported to '{output_file_path}'")
